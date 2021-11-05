018392ebf741dba82706a46790986315d321c8b1
jseo9732 <jseo9732@gmail.com>
2021-11-05 19:38:36 +0900

    Design: 전체적인 디자인 수정

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,21 +1,40 @@
+import { useHistory } from "react-router";
 import "./ChatRoom.css";
 
 export default function ChatRoom() {
+  const history = useHistory();
+  const onBackClick = () => {
+    history.push("/");
+  };
+
   return (
     <div className="chatRoomContainer">
       <div className="chatRoomTopBar">
-        <div className="chatRoomBack">뒤로가기</div>
+        <div className="chatRoomBackContainer">
+          <img
+            onClick={onBackClick}
+            className="chatRoomImg"
+            src="/image/chevron-left-solid.svg"
+            alt="뒤로가기"
+          />
+        </div>
         <div className="chatRoomTitleContainer">
           <span className="chatRoomTitle">들어오세요~</span>
-          <span className="chatRoomNum">3</span>
+          <span className="chatRoomNum">(3)</span>
         </div>
 
-        <div className="chatRoomMenu">햄버거</div>
+        <div className="chatRoomMenu">
+          <img
+            className="chatRoomImg"
+            src="/image/bars-solid.svg"
+            alt="채팅 메뉴"
+          />
+        </div>
       </div>
       <div className="chatContents"></div>
       <div className="chatInputContainer">
-        <form>
-          <input className="chatInput" type="text" />
+        <form className="chatInputForm">
+          <textarea className="chatInput" type="text" />
           <button className="inputBtn" type="submit">
             전송
           </button>
