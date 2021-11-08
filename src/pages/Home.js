8872f7cdc5a18659e5a265941e06f089ab7a49f7
jseo9732 <jseo9732@gmail.com>
2021-11-08 16:36:42 +0900

    Design: 추가 로그아웃 버튼

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="homeContainer">
       <div className="homeTitle">AllChat</div>
+      <button className="LogoutBtn">로그아웃</button>
     </div>
   );
 }
