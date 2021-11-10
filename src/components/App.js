811cee16575a9c2af97519f14bdbf99e5623857b
jseo9732 <jseo9732@gmail.com>
2021-11-11 01:39:39 +0900

    Feat: 추가 로그인 기능

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import AppRouter from "./Router";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function getCookie(cName) {
+    cName = cName + "=";
+    let cookieData = document.cookie;
+    let start = cookieData.indexOf(cName);
+    let cValue = "";
+    if (start !== -1) {
+      start += cName.length;
+      var end = cookieData.indexOf(";", start);
+      if (end === -1) end = cookieData.length;
+      cValue = cookieData.substring(start, end);
+    }
+    return unescape(cValue);
+  }
+
+  useEffect(() => {
+    const jwtToken = getCookie("isLogin");
+    setIsLoggedIn(jwtToken);
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn="true" />
+      <AppRouter isLoggedIn={isLoggedIn} />
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,60 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import axios from "axios";
 
 export default function Login() {
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+  const [loginFail, setLoginFail] = useState(false);
+
+  function setCookie(cName, cValue, cDay) {
+    const expire = new Date();
+    expire.setDate(expire.getDate() + cDay);
+    let cookies = cName + "=" + escape(cValue) + "; path=/ ";
+    if (typeof cDay != "undefined")
+      cookies += ";expires=" + expire.toGMTString() + ";";
+    document.cookie = cookies;
+  }
+
+  const onUserIdChange = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const onUserPwChange = (e) => {
+    setUserPw(e.target.value);
+  };
+
+  const onLoginClick = (e) => {
+    e.preventDefault();
+
+    if (userId === "" || userPw === "") {
+      window.alert("아이디와 비밀번호를 입력해주세요.");
+    } else {
+      axios(
+        "http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          data: {
+            username: userId,
+            password: userPw,
+          },
+        }
+      )
+        .then((res) => {
+          const accessToken = res.data.jwtToken;
+          setCookie("isLogin", accessToken, 30);
+          document.location.href = "/";
+        })
+        .catch((err) => {
+          setLoginFail(!err.response.data.data);
+        });
+    }
+  };
+
   return (
     <div className="loginContainer">
       <h1 className="title">AllChat</h1>
@@ -13,6 +66,8 @@ export default function Login() {
           type="text"
           placeholder="ID"
           required
+          value={userId}
+          onChange={onUserIdChange}
         />
         <input
           className="loginInput"
@@ -21,8 +76,17 @@ export default function Login() {
           type="password"
           placeholder="Password"
           required
+          value={userPw}
+          onChange={onUserPwChange}
         />
-        <button className="loginBtn" type="submit">
+        {loginFail && (
+          <div className="Warning">
+            아이디 또는 비밀번호가 잘못 입력 되었습니다. <br />
+            <b>아이디</b>와 <b>비밀번호</b>를 정확히 입력해 주세요.
+          </div>
+        )}
+
+        <button className="loginBtn" type="submit" onClick={onLoginClick}>
           로그인
         </button>
         <a
