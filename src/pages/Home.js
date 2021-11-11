bef5b618a2dbb33e1e53ac9f33184195593e2dd6
jseo9732 <jseo9732@gmail.com>
2021-11-11 17:57:34 +0900

    Feat: 추가 로그아웃 기능

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import "./Home.css";
+import Cookies from "universal-cookie";
 
 export default function Home() {
+  const cookies = new Cookies();
+
+  const onLogoutClick = () => {
+    cookies.remove("myToken");
+    document.location.href = "/";
+  };
   return (
     <div className="homeContainer">
       <div className="homeTitle">AllChat</div>
-      <button className="LogoutBtn">로그아웃</button>
+      <button className="LogoutBtn" onClick={onLogoutClick}>
+        로그아웃
+      </button>
     </div>
   );
 }
