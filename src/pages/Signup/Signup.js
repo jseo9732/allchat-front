2e82e58c8ec88f37e6ab62dda2faa6858e2b7264
jseo9732 <jseo9732@gmail.com>
2021-11-16 20:06:04 +0900

    Refactor: 개선 회원가입 코드

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -7,30 +7,30 @@ import { useHistory } from "react-router";
 export default function Signup() {
   const history = useHistory();
 
+  // 아이디 입력 창 관련
   const [userId, setUserId] = useState("");
   const [isUserIdNull, setIsUserIdNull] = useState(false);
   const [isUserIdLen, setIsUserIdLen] = useState(false);
   const [isUserIdDup, setIsUserIdDup] = useState(false);
   const [isNotUserIdDup, setIsNotUserIdDup] = useState(false);
 
-  const onUserIdBlur = (e) => {
-    const inputId = e.target.value;
-    setIsUserIdNull(inputId === "");
-    setIsUserIdLen(inputId !== "" && inputId.length < 2);
-    if (inputId.length >= 2) {
-      userIdDuplicate(inputId);
+  const onUserIdChange = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const onUserIdBlur = () => {
+    setIsUserIdNull(userId === "");
+    setIsUserIdLen(userId.length !== 0 && userId.length < 2);
+    if (userId.length >= 2) {
+      userIdDuplicate(userId);
     } else {
       setIsUserIdDup(false);
       setIsNotUserIdDup(false);
     }
   };
 
-  const onUserIdChange = (e) => {
-    setUserId(e.target.value);
-  };
-
-  const userIdDuplicate = (inputId) => {
-    axios(
+  const userIdDuplicate = async (userId) => {
+    await axios(
       "http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/auth/duplicate",
       {
         method: "POST",
@@ -38,7 +38,7 @@ export default function Signup() {
           "Content-Type": "application/json",
         },
         data: {
-          username: inputId,
+          username: userId,
         },
       }
     )
@@ -47,36 +47,46 @@ export default function Signup() {
         setIsNotUserIdDup(res.data.data);
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err.response);
       });
   };
 
+  // 비밀번호 입력 창 관련
   const [userPw, setUserPw] = useState("");
-  const [userPwCheck, setUserPwCheck] = useState("");
   const [isUserPwNull, setIsUserPwNull] = useState(false);
-  const [isUserPwCheckNull, setIsUserPwCheckNull] = useState(false);
   const [isUserPwLen, setIsUserPwLen] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
 
   const onUserPwChange = (e) => {
     setUserPw(e.target.value);
   };
 
+  const onUserPwBlur = () => {
+    setIsUserPwNull(userPw === "");
+    setIsUserPwLen(userPw.length !== 0 && userPw.length < 4);
+  };
+
+  // 비밀번호 확인 입력 창 관련
+  const [userPwCheck, setUserPwCheck] = useState("");
+  const [isUserPwCheckNull, setIsUserPwCheckNull] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordSuccess, setPasswordSuccess] = useState(false);
+
   const onUserPwCheckChange = (e) => {
     setUserPwCheck(e.target.value);
   };
 
-  const onUserPwBlur = (e) => {
-    setIsUserPwNull(e.target.value === "");
-    setIsUserPwLen(e.target.value.length !== 0 && e.target.value.length < 4);
+  const onPWCheckBlur = () => {
+    setIsUserPwCheckNull(userPwCheck === "");
+    setPasswordError(userPwCheck !== userPw);
+    setPasswordSuccess(userPwCheck === userPw);
+    if (userPwCheck === "") {
+      setPasswordError(false);
+      setPasswordSuccess(false);
+    }
   };
 
-  const onPWCheckBlur = (e) => {
-    setIsUserPwCheckNull(e.target.value === "");
-    setPasswordError(e.target.value !== userPw);
-  };
-
-  const onSignupClick = (e) => {
+  // 회원가입 버튼 클릭 시
+  const onSignupClick = async (e) => {
     e.preventDefault();
     setIsUserIdNull(userId === "");
     setIsUserPwNull(userPw === "");
@@ -91,7 +101,7 @@ export default function Signup() {
       !isUserPwCheckNull &&
       !passwordError
     ) {
-      axios(
+      await axios(
         "http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/auth/signup",
         {
           method: "POST",
@@ -109,7 +119,7 @@ export default function Signup() {
           history.push("/");
         })
         .catch((err) => {
-          console.log(err);
+          console.log(err.response);
         });
     }
   };
@@ -119,14 +129,15 @@ export default function Signup() {
       <h1 className="Signuptitle">회원가입</h1>
       <form className="SignupForm">
         <div className="SignupUserId">
-          <label htmlFor="user-id">아이디</label>
+          <label>아이디</label>
           <input
-            value={userId}
-            name="user-id"
+            type="text"
+            placeholder="사용할 아이디를 입력하세요"
             required
+            autoFocus
+            value={userId}
             minLength="2"
             maxLength="20"
-            placeholder="사용할 아이디를 입력하세요"
             onChange={onUserIdChange}
             onBlur={onUserIdBlur}
           />
@@ -136,39 +147,40 @@ export default function Signup() {
             <div className="Warning">이미 사용중인 아이디입니다.</div>
           )}
           {isNotUserIdDup && (
-            <div className="Warning">사용 가능한 아이디입니다.</div>
+            <div className="Success">사용 가능한 아이디입니다.</div>
           )}
         </div>
         <div className="SignupUserPw">
-          <label htmlFor="user-pw">비밀번호</label>
+          <label>비밀번호</label>
           <input
-            placeholder="사용할 비밀번호를 입력하세요"
-            name="user-pw"
             type="password"
+            placeholder="사용할 비밀번호를 입력하세요"
             required
+            value={userPw}
             minLength="4"
             maxLength="16"
-            value={userPw}
             onChange={onUserPwChange}
             onBlur={onUserPwBlur}
           />
           {isUserPwNull && <div className="Warning">필수 정보입니다.</div>}
           {isUserPwLen && <div className="Warning">4~16자만 가능합니다.</div>}
-          <label htmlFor="user-password-check">비밀번호 재확인</label>
+          <label>비밀번호 재확인</label>
           <input
-            placeholder="비밀번호를 재입력하세요"
-            name="user-password-check"
             type="password"
+            placeholder="사용할 비밀번호를 재입력하세요"
             required
+            value={userPwCheck}
             minLength="4"
             maxLength="16"
-            value={userPwCheck}
             onChange={onUserPwCheckChange}
             onBlur={onPWCheckBlur}
           />
           {isUserPwCheckNull && <div className="Warning">필수 정보입니다.</div>}
           {passwordError && (
             <div className="Warning">비밀번호가 일치하지 않습니다.</div>
+          )}
+          {passwordSuccess && (
+            <div className="Success">비밀번호가 일치합니다.</div>
           )}
         </div>
         <div>
