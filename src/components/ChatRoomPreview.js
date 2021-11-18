93c75608de1bc5cae65360a015bc2dccf7d8ad7e
jseo9732 <jseo9732@gmail.com>
2021-11-19 05:54:09 +0900

    Refactor: 개선 채팅목록 미리보기 요청 코드

--- src/components/ChatRoomPreview.js
+++ src/components/ChatRoomPreview.js
@@ -16,54 +16,62 @@ export default function ChatRoomPreview({
 }) {
   // const cookies = new Cookies();
 
+  // 1. 채팅 참여 요청
   const joinChat = async () => {
-    if (participantState === false) {
-      await axios(
-        "http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/joins",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: jwtToken,
-          },
-          data: {
-            userId: userId,
-            chatRoomId: chatRoomId,
-          },
+    await axios(
+      "http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/joins",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: jwtToken,
+        },
+        data: {
+          userId: userId,
+          chatRoomId: chatRoomId,
+        },
+      }
+    )
+      .then((res) => {
+        refreshAllList();
+        refreshEnterList();
+        // 2. 채팅방 입장 시간 조회
+        axios
+          .get(
+            `http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/chatrooms/${chatRoomId}/joins/time?userId=${userId}`
+          )
+          .then((res) => {
+            onJoinSave(res.data.data.username);
+          });
+      })
+      .catch((err) => {
+        if (err.response.data.error === "Unauthorized") {
+          alert("로그인 후 다시 이용해주세요");
+          // cookies.remove("jwtToken");
+          // cookies.remove("userId");
+          // document.location.href = "/";
+        } else {
+          console.log(err.response);
         }
-      )
-        .then((res) => {
-          refreshAllList();
-          refreshEnterList();
-        })
-        .catch((err) => {
-          if (err.response.data.error === "Unauthorized") {
-            alert("로그인 후 다시 이용해주세요");
-            // cookies.remove("jwtToken");
-            // cookies.remove("userId");
-            // document.location.href = "/";
-          } else {
-            console.log(err.response);
-          }
-        });
-    }
+      });
   };
 
-  // const [chatData, setChatData] = useState([]);
-  // const getChatMsg = async () => {
-  //   await axios(
-  //     `http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/chatrooms/${chatRoomId}/joins/time?userId=${userId}`,
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: jwtToken,
-  //       },
-  //     }
-  //   ).then((res) => {
-  //     setChatData(res.data.data);
-  //   });
-  // };
+  // 3. 참여 메세지 저장
+  const onJoinSave = async (username) => {
+    await axios(
+      "http://eballchatchatting-env.eba-gfegivem.ap-northeast-2.elasticbeanstalk.com/chats/notifications",
+      {
+        method: "POST",
+        data: {
+          participant: username,
+          roomId: chatRoomId,
+          join: true,
+        },
+      }
+    ).then((res) => {
+      console.log(res.data);
+    });
+  };
 
   return (
     <Link
@@ -72,9 +80,9 @@ export default function ChatRoomPreview({
           ? {
               pathname: `/chatRoom/${chatRoomId}`,
               state: {
+                chatRoomId,
                 masterId,
                 participantCount,
-                participantState,
                 title,
                 userId,
                 jwtToken,
