7342ec66cde033167bba4e723bdae5a74f7defe4
jseo9732 <jseo9732@gmail.com>
2021-11-19 23:16:43 +0900

    Refactor: 개선 채팅방 코드

--- src/pages/ChatRoom/ChatRoom.js
+++ src/pages/ChatRoom/ChatRoom.js
@@ -1,15 +1,17 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router";
 import GetChatContents from "../../components/GetChatContents";
+import ChatInput from "../../components/ChatInput";
 import ChatSideMenu from "../../components/ChatSideMenu";
 import "./ChatRoom.css";
 
 export default function ChatRoom({
-  location: { state, refreshAllList, refreshEnterList },
+  location: {
+    state: { chatRoomId, masterId, participantCount, title, userId, jwtToken },
+    refreshAllList,
+    refreshEnterList,
+  },
 }) {
-  const { chatRoomId, masterId, participantCount, title, userId, jwtToken } =
-    state;
   const history = useHistory();
 
   const onBackClick = () => {
@@ -22,21 +24,6 @@ export default function ChatRoom({
   const onSideMenuClick = () => {
     setShowSideMenu(!showSideMenu);
   };
-
-
-  const [joinData, setJoinData] = useState([]);
-  useEffect(() => {
-    // 채팅방 입장 시간 조회
-    axios
-      .get(
-        `http://eballchatmain-env.eba-ky3tiuhm.ap-northeast-2.elasticbeanstalk.com/chatrooms/${chatRoomId}/joins/time?userId=${userId}`
-      )
-      .then((res) => {
-        setJoinData(res.data.data);
-      });
-    setShowSideMenu(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chatRoomId]);
 
   return (
     <>
@@ -71,7 +58,6 @@ export default function ChatRoom({
             chatRoomId={chatRoomId}
             userId={userId}
             jwtToken={jwtToken}
-            joinData={joinData}
           />
           <ChatInput chatRoomId={chatRoomId} userId={userId} />
           <ChatSideMenu
